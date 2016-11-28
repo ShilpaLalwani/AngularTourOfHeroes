8c76b3a0f86e5392274c8c53176d0b481ab03d64
Siddharth Ajmera <siddharth.ajmera@t-mobile.com>
2016-11-28 15:39:49 +0530

    Changes for routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,43 +2,30 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-import { AppComponent }        from './app.component';
+import { AppComponent }   from './app.component';
+import { HeroService }    from './hero.service';
+import { AppRoutingModule } from './app-routing.module';   
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { DashboardComponent }     from './dashboard.component';
-import { HeroService }         from './hero.service';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-	  {
-	    path: 'heroes',
-	    component: HeroesComponent
-	  },
-	  {
-		 path: 'dashboard',
-		 component: DashboardComponent
-	  },
-	  {
-		  path: '',
-		  redirectTo: '/dashboard',
-		  pathMatch: 'full'
-		},
-	   {
-		  path: 'detail/:id',
-		  component: HeroDetailComponent
-		},
-
-	])
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    HttpModule,
   ],
   declarations: [
     AppComponent,
     HeroDetailComponent,
-   	HeroesComponent,
-   	DashboardComponent
+    HeroesComponent,
+    DashboardComponent
   ],
   providers: [
     HeroService
